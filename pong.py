fd1904ec19f265925a7caf9b49290ffcb0940588
Aichi <aichitsuchihira@gmail.com>
2019-11-12 11:32:26 +1100

    Added borders to the top and bottom edges

--- pong.py
+++ pong.py
@@ -37,10 +37,60 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 0.5
+ball.dy = -0.5
+
+
+def paddleAUp():
+    # A function to move the paddle
+    y = paddleA.ycor()
+    y += 20
+    paddleA.sety(y)
+
+
+def paddleADown():
+    # A function to move the paddle
+    y = paddleA.ycor()
+    y -= 20
+    paddleA.sety(y)
+
+
+def paddleBUp():
+    # A function to move the paddle
+    y = paddleB.ycor()
+    y += 20
+    paddleB.sety(y)
+
+
+def paddleBDown():
+    # A function to move the paddle
+    y = paddleB.ycor()
+    y -= 20
+    paddleB.sety(y)
+
+
+# Keyboard binding
+win.listen()
+win.onkeypress(paddleAUp, "w")
+win.onkeypress(paddleADown, "s")
+win.onkeypress(paddleBUp, "Up")
+win.onkeypress(paddleBDown, "Down")
 
 
 # Main game loop
-
 while True:
     # Update the screen
     win.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
